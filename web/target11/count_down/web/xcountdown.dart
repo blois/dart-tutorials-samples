3a41d1e6557ba54b399f70fcce82eb53cb4689e3
Mary Campione <mcampione@google.com>
2013-06-06 16:21:21 -0700

    added check for unsupported idb, ugly code

--- web/target11/count_down/web/xcountdown.dart
+++ web/target11/count_down/web/xcountdown.dart
@@ -3,21 +3,20 @@ import 'dart:html';
 import 'dart:async';
 import 'dart:indexed_db';
 
-// Observe timeRemaining. Used to update each milestone display.
+// Observe timeRemaining. Used to update each milestone display each second.
 @observable String timeRemaining = '';
 
 // Observe msg. It displays a message for the user.
 @observable String msg = '';
 
 // A class to hold the milestone info.
-// XX: should probably have a specialized list so as to override contains();
 @observable class Milestone {
   String name;
-  String date;
+  String dateAndTime;
   String display;
   
-  Milestone(this.name, this.date, this.display);
-  String toString() => this.name + ' ' + this.date + ' ' + this.display;
+  Milestone(this.name, this.dateAndTime, this.display);
+  String toString() => this.name + ' ' + this.dateAndTime + ' ' + this.display;
 }
 
 class CounterComponent extends WebComponent {
@@ -28,17 +27,20 @@ class CounterComponent extends WebComponent {
   String newMilestoneTime = '00:00:00';
   
   // List of milestones to which to count down
+  // XX: should probably have a specialized list to override contains();
   @observable List<Milestone> milestones = toObservable(new List());
   
   // Fires an event every second to update displays.
-  Timer timer;
+  // Is null when no milestones to count.
+  Timer timer = null;
   
   // The indexed database to store the countdown timers. Provided by the window.
   Database indexedDB;
   String storeName = 'newcountdown'; // the name 
   int dbVersion;
   
-  
+  bool idbAvailable = IdbFactory.supported;
+
   /*
    * Initializes the database connection.
    * Opens the database called 'myAwesomeDatabase'.
@@ -48,6 +50,8 @@ class CounterComponent extends WebComponent {
   // Overrides WebComponent inserted() method,
   // XX: is there a super.inserted() that I should be calling?
   inserted() {
+    if (!idbAvailable) return;
+
     // Open the Database.
     window.indexedDB.open('myAwesomeDatabase', version: 7,
         onUpgradeNeeded: (e) {
@@ -60,7 +64,7 @@ class CounterComponent extends WebComponent {
           indexedDB = db;
           initializeFromDB();
           return true; // XX: why am I returning true here?
-        });  
+        });
   }
 
   // Called from inserted(), gets all items from the database
@@ -83,6 +87,8 @@ class CounterComponent extends WebComponent {
   // Show button click handler.
   // Prints the number of milestones currently in the database.
   void showMeTheMoney() {
+    if (!idbAvailable) return;
+    
     Transaction t = indexedDB.transaction(storeName, 'readonly');
     t.objectStore(storeName).count()
     .then((count) {
@@ -95,6 +101,8 @@ class CounterComponent extends WebComponent {
   // Clear button click handler.
   // Removes all milestones from the internal list and from the database
   void clearDatabase() {
+    if (!idbAvailable) { milestones.clear(); msg =''; return; }
+    
     Transaction t = indexedDB.transaction(storeName, 'readwrite');
     t.objectStore(storeName).clear()
     .then((request) {
@@ -124,7 +132,7 @@ class CounterComponent extends WebComponent {
     // Make sure milestone is in the future, and not in the past.
     var now = new DateTime.now();
     var milestoneTime = DateTime.parse(str);
-    if (now.compareTo(milestoneTime) < 0) { // If milestone is in the future, add milestone.
+    if (milestoneTime.isAfter(now)) { // If milestone is in the future, add milestone.
       _addMilestone(newMilestoneName, str);
       print('adding $newMilestoneName $str');
       msg = '';
@@ -136,6 +144,13 @@ class CounterComponent extends WebComponent {
   // Called from initializeFromDB and from + button click handler.
   void _addMilestone(String name, String dateAndTime) {
     print('in _addMilestone');
+    print(dateAndTime);
+    
+    if (!idbAvailable) {
+      milestones.add(new Milestone(name, dateAndTime, name));
+      startMilestoneTimer();
+      return;
+    }
     
     Transaction t = indexedDB.transaction(storeName, 'readwrite');
     t.objectStore(storeName).put(dateAndTime, name) /* it's value first, then key. who's idea was that? */
@@ -151,6 +166,17 @@ class CounterComponent extends WebComponent {
   void removeMilestone(int index) {
     print('in removeMilestone');
     print(index);
+
+    if (!idbAvailable) {
+      milestones.removeAt(index);
+      msg = '';
+      
+      // Turn off the timer if no more milestones to count down.
+      if (milestones.length == 0) {
+        timer.cancel();
+        timer = null;
+      }
+    }
     
     Transaction t = indexedDB.transaction(storeName, 'readwrite');
     t.objectStore(storeName).delete(milestones[index].name)
@@ -177,12 +203,12 @@ class CounterComponent extends WebComponent {
     if (timer == null) {
       // The timer goes off every second
       var oneSecond = new Duration(seconds:1);
-      timer = new Timer.periodic(oneSecond, updateTime);
+      timer = new Timer.periodic(oneSecond, updateDisplays);
     }
   }
   
   // Update the display for each milestone.
-  void updateTime(Timer _) {
+  void updateDisplays(Timer _) {
     // What time is it now?
     var now = new DateTime.now();
 
@@ -190,38 +216,44 @@ class CounterComponent extends WebComponent {
     for (int i = 0; i < milestones.length; i++) {
 
       // If milestone hasn't already passed...
-      if (milestones[i].display != 'Congratulations!') {
-        final int secondsInDay = 60*60*24;
-        final int secondsInHour = 60*60;
-        
+      if (!milestones[i].display.startsWith('Huzzah')) {
         // What time is the milestone?
-        var milestoneTime = DateTime.parse(milestones[i].date);
+        var milestoneTime = DateTime.parse(milestones[i].dateAndTime);
         
         // What is the difference between now and milestone in seconds?
         int secs = milestoneTime.difference(now).inSeconds;
-        
-        // Milestone has JUST occurred.
+       
         if (secs <= 0) {
-          timeRemaining = 'Congratulations!';
+          // Milestone has JUST occurred.
+          timeRemaining = 'Huzzah for ${milestones[i].name}!';
           milestones[i].display = timeRemaining;
         } else {
-          // Calculate days, hours, and minutes remaining.
-          int d=0, h=0, m=0;
-          if (secs >= secondsInDay) { d = secs ~/ secondsInDay; secs = secs % secondsInDay; }
-          if (secs >= secondsInHour) { h = secs ~/ secondsInHour; secs = secs % secondsInHour; }
-          if (secs >= 60) { m = secs ~/ 60; secs = secs % 60; }
-         
-          // Format individual pieces of the display string.
-          String days = (d == 0) ? '' : (d <= 9) ? '0$d Days' : '$d Days';
-          String hours = (h == 0) ? '' : (h <= 9) ? '0$h Hours' : '$h Hours';
-          String minutes = (m == 0) ? '' : (m <= 9) ? '0$m Minutes' : '$m Minutes';
-          String seconds = (secs <= 9) ? '0$secs Seconds' : '$secs Seconds';
-          
-          // Display it.
-          timeRemaining = '$days $hours $minutes $seconds';          
+          // Still counting down...display it.
+          // These are observable strings, so web page gets automatically updated.
+          timeRemaining = formatDisplayString(secs);
           milestones[i].display = timeRemaining + ' until ${milestones[i].name}';
         } // end if-else
       } // end if
     } // end for loop
-  } // end updateTime
-}
+  } // end updateDisplays
+  
+  String formatDisplayString(int secs) {
+    // Some constants.
+    final int secondsPerDay = 60*60*24;
+    final int secondsPerHour = 60*60;
+
+    // Calculate days, hours, and minutes remaining.
+    int d=0, h=0, m=0;
+    if (secs >= secondsPerDay) { d = secs ~/ secondsPerDay; secs = secs % secondsPerDay; }
+    if (secs >= secondsPerHour) { h = secs ~/ secondsPerHour; secs = secs % secondsPerHour; }
+    if (secs >= 60) { m = secs ~/ 60; secs = secs % 60; }
+    
+    // Format individual pieces of the display string.
+    String days = (d == 0) ? '' : '$d Days';
+    String hours = (h == 0) ? '' : '$h Hours';
+    String minutes = (m == 0) ? '' : '$m Minutes';
+    String seconds = '$secs Seconds';
+    
+    return '$days $hours $minutes $seconds';
+  }
+} // end class
